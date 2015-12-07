15f3361523ecc000498bf0a3980d4340cf388469
warren.matt <warren.matt@hotmail.com>
2015-12-07 18:29:48 -0500

    finished form

--- TriangleForm/Form1.Designer.cs
+++ TriangleForm/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbRightAngle = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSideA = new System.Windows.Forms.TextBox();
+            this.txtSideB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAngleA = new System.Windows.Forms.TextBox();
+            this.txtAngleB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.btnDoStuff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbRightAngle
+            // 
+            this.cbRightAngle.AutoSize = true;
+            this.cbRightAngle.Location = new System.Drawing.Point(10, 12);
+            this.cbRightAngle.Name = "cbRightAngle";
+            this.cbRightAngle.Size = new System.Drawing.Size(93, 17);
+            this.cbRightAngle.TabIndex = 0;
+            this.cbRightAngle.Text = "Right Angled?";
+            this.cbRightAngle.UseVisualStyleBackColor = true;
+            this.cbRightAngle.CheckedChanged += new System.EventHandler(this.cbRightAngle_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Side A";
+            // 
+            // txtSideA
+            // 
+            this.txtSideA.Location = new System.Drawing.Point(64, 35);
+            this.txtSideA.Name = "txtSideA";
+            this.txtSideA.Size = new System.Drawing.Size(53, 20);
+            this.txtSideA.TabIndex = 2;
+            // 
+            // txtSideB
+            // 
+            this.txtSideB.Location = new System.Drawing.Point(64, 61);
+            this.txtSideB.Name = "txtSideB";
+            this.txtSideB.Size = new System.Drawing.Size(53, 20);
+            this.txtSideB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Side B";
+            // 
+            // txtAngleA
+            // 
+            this.txtAngleA.Location = new System.Drawing.Point(64, 105);
+            this.txtAngleA.Name = "txtAngleA";
+            this.txtAngleA.Size = new System.Drawing.Size(53, 20);
+            this.txtAngleA.TabIndex = 6;
+            // 
+            // txtAngleB
+            // 
+            this.txtAngleB.Location = new System.Drawing.Point(64, 131);
+            this.txtAngleB.Name = "txtAngleB";
+            this.txtAngleB.Size = new System.Drawing.Size(53, 20);
+            this.txtAngleB.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Angle A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Angle B";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(10, 224);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(147, 89);
+            this.txtResults.TabIndex = 10;
+            // 
+            // btnDoStuff
+            // 
+            this.btnDoStuff.Location = new System.Drawing.Point(12, 172);
+            this.btnDoStuff.Name = "btnDoStuff";
+            this.btnDoStuff.Size = new System.Drawing.Size(145, 23);
+            this.btnDoStuff.TabIndex = 11;
+            this.btnDoStuff.Text = "Do Stuff";
+            this.btnDoStuff.UseVisualStyleBackColor = true;
+            this.btnDoStuff.Click += new System.EventHandler(this.btnDoStuff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(168, 316);
+            this.Controls.Add(this.btnDoStuff);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAngleB);
+            this.Controls.Add(this.txtAngleA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSideB);
+            this.Controls.Add(this.txtSideA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbRightAngle);
+            this.Name = "Form1";
+            this.Text = "Triangles...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox cbRightAngle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSideA;
+        private System.Windows.Forms.TextBox txtSideB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAngleA;
+        private System.Windows.Forms.TextBox txtAngleB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button btnDoStuff;
     }
 }
 
--- TriangleForm/Form1.cs
+++ TriangleForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Triangle;
 
 namespace TriangleForm
 {
@@ -15,6 +16,58 @@ namespace TriangleForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDoStuff_Click(object sender, EventArgs e)
+        {
+            if (cbRightAngle.Checked)
+            {
+                //right angle triangle
+                if(txtSideA.Text!="" && txtSideB.Text != "")
+                {
+                    Triangle.Triangle tri = new Triangle.Triangle(Convert.ToDouble(txtSideA.Text), Convert.ToDouble(txtSideB.Text));
+                    txtResults.Text = "Side C = " + Convert.ToString(tri.GetSideC()) +
+                                      "\r\nArea = " + Convert.ToString(tri.CalculateArea());
+                }
+                else
+                {
+                    MessageBox.Show("Need sides to get area and other side.");
+                }
+            }
+            else
+            {
+                //not right angle triangle, can only do angle things
+                //therefore find angles
+                if (txtAngleA.Text != "" && txtAngleB.Text != "")
+                {
+                    Triangle.Triangle tri = new Triangle.Triangle(Convert.ToDouble(txtAngleA.Text), Convert.ToDouble(txtAngleB.Text), 0);
+                    txtResults.Text = "Missing angle = " + Convert.ToString(tri.GetMissingAngle('c'));
+                }
+                else
+                {
+                    MessageBox.Show("Need angles to get the missing angle.");
+                }
+            }
+        }
+
+        private void cbRightAngle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbRightAngle.Checked)
+            {
+                btnDoStuff.Text = "Side C and Area";
+                txtAngleA.Enabled = false;
+                txtAngleB.Enabled = false;
+                txtSideA.Enabled = true;
+                txtSideB.Enabled = true;
+            }
+            else
+            {
+                btnDoStuff.Text = "Find Missing Angle";
+                txtAngleA.Enabled = true;
+                txtAngleB.Enabled = true;
+                txtSideA.Enabled = false;
+                txtSideB.Enabled = false;
+            }
         }
     }
 }
