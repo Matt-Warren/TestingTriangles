1c1eca3e5e487d5dcaab61d958a62cf476e26443
warren.matt <warren.matt@hotmail.com>
2015-12-07 16:36:46 -0500

    first commit

---
+++ Triangle/Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public double sideA;
+        public double sideB;
+        public double sideC;
+
+        public double area;
+
+        public double angleAB;
+        public double angleAC;
+        public double angleBC;
+
+        public bool rightAngled;
+
+        public Triangle()
+        {
+
+        }
+
+        public Triangle(double sideA, double sideB)
+        {
+
+        }
+
+        public void CalculateArea()
+        {
+
+        }
+
+        public double GetMissingAngle()
+        {
+            double missingAngle = 0;
+
+            return missingAngle;
+        }
+
+    }
+}
