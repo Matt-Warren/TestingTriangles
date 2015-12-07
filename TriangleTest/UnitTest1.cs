e59ba06ac9f90a2aabf107923d9c114bb440f0d5
warren.matt <warren.matt@hotmail.com>
2015-12-07 18:04:25 -0500

    test for getmissingangle

--- TriangleTest/UnitTest1.cs
+++ TriangleTest/UnitTest1.cs
@@ -8,10 +8,21 @@ namespace TriangleTest
     public class UnitTest1
     {
         [TestMethod]
+        public void Test_MissingAngle()
+        {
+            Triangle.Triangle tri = new Triangle.Triangle();
+            tri.angleA = 50;
+            tri.angleC = 2;
+            double status = tri.GetMissingAngle('b');
+            Assert.AreEqual(Math.Round(status, 4), status);
+        }
+
+
+        [TestMethod]
         public void Test_Cosine()
         {
             Triangle.Triangle tri = new Triangle.Triangle(20, 10);
-            tri.angleAB = 56;
+            tri.angleC = 56;
             double status = tri.Cosine('c');
             Assert.AreEqual(Math.Round(16.62296, 4), status);
         }
