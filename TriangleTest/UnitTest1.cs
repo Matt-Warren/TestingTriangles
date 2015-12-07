e0efdff7b6ef45e6e7d977c0b36c1306af47545b
warren.matt <warren.matt@hotmail.com>
2015-12-07 18:35:14 -0500

    added comments and final tests

--- TriangleTest/UnitTest1.cs
+++ TriangleTest/UnitTest1.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+* FILE : UnitTest1.cs
+* PROGRAMMER : Matt Warren
+* FIRST VERSION : 2015-12-07
+* DESCRIPTION :
+* The functions in this file are used to test the functions in the Triangle class
+*/
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Triangle;
 
@@ -7,6 +15,21 @@ namespace TriangleTest
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
+        public void Test_SideConstructor()
+        {
+            Triangle.Triangle tri = new Triangle.Triangle(15, 20);
+            Assert.AreEqual(tri.sideB, 20);
+        }
+
+        [TestMethod]
+        public void Test_AngleConstructor()
+        {
+            Triangle.Triangle tri = new Triangle.Triangle(15, 20, 0);
+            Assert.AreEqual(tri.angleA, 15);
+        }
+
+
         [TestMethod]
         public void Test_MissingAngle()
         {
